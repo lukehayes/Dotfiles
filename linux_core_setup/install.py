c49b68ca6906eb91aee1828f9da5c9550e844edb
lukehayes <lukepurplehayes@gmail.com>
2023-01-04 17:52:32 +0000

    Added missing sudo from pacman calls

--- linux_core_setup/install.py
+++ linux_core_setup/install.py
@@ -9,13 +9,13 @@ print("Runing grub-mkconfig...")
 os.system("grub-mkconfig -o /boot/grub/grub.cfg")
 
 print("Setting up archlinux-keyring...")
-os.system("pacman -Sy archlinux-keyring")
+os.system("sudo acman -Sy archlinux-keyring")
 
 print("Copying pacman.conf...")
 os.system("sudo cp pacman.conf /etc/")
 
 print("Updating System...")
-os.system("pacman -Su")
+os.system("sudo pacman -Su")
 
 print("Copying .Xresources file to $HOME ...")
 os.system("sudo cp .Xresources $HOME")
