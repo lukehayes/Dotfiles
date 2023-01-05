4eee7883bfc23ccdd5340b17129f1e3d5d21f8ec
lukehayes <lukepurplehayes@gmail.com>
2023-01-05 18:02:29 +0000

    dotfiles_install.py now clones my vim config from git.

--- dotfiles_install.py
+++ dotfiles_install.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 
 ## 
-# Script installs all of the dotfiles that I use into $HOME/.config/symlinks folder.
+# Script installs all of the dotfiles that I use into $HOME/.config/dotfiles folder.
 ## 
 
 import os
 
 # Variables
 # 
-symlink_dir_path = "$HOME/.config/symlinks"
-zsh_dir_path = symlink_dir_path + "/zsh"
-vim_dir_path = symlink_dir_path + "/vim"
+dotfile_dir_path = "$HOME/.config/dotfiles"
+zsh_dir_path = dotfile_dir_path + "/zsh"
+vim_dir_path = dotfile_dir_path + "/vim"
 
 # Helper functions
 # 
@@ -22,9 +22,17 @@ def copy_file(src, dest):
     print("Copying {} to {}".format(src,dest))
     os.system("cp {} {}".format(src,dest))
 
+def clone_repo(repo, dest):
+    print("Git cloning {} to {}".format(repo,dest))
+    os.system("git clone {} {}".format(repo,dest))
+
+def create_symlink(src, dest):
+    print("Creating dotfile {} to {}".format(src,dest))
+    os.system("ln -s {} {}".format(src,dest))
+
 # Setup config files
 # 
-create_dir(symlink_dir_path)
+create_dir(dotfile_dir_path)
 
 # ZSH
 # 
@@ -32,6 +40,10 @@ create_dir(zsh_dir_path)
 copy_file(".zshrc", zsh_dir_path)
 copy_file("aliases", zsh_dir_path)
 copy_file("variables", zsh_dir_path)
+create_symlink(zsh_dir_path + "/.zshrc", "$HOME/.zshrc")
 
-
-
+# VIM
+# 
+create_dir(vim_dir_path)
+clone_repo("git@github.com:lukehayes/VimConfig.git", vim_dir_path)
+create_symlink(vim_dir_path + "/init.vim", "$HOME/.vimrc")
