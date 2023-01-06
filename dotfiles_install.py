6ade3a1fa446cc3ea61a4391568b17aa27068d87
lukehayes <lukepurplehayes@gmail.com>
2023-01-06 17:40:07 +0000

    Added font installation to dotfiles_install.py

--- dotfiles_install.py
+++ dotfiles_install.py
@@ -12,6 +12,7 @@ config_dir_path  = "$HOME/.config"
 dotfile_dir_path = config_dir_path  + "/dotfiles"
 avim_dir_path    = config_dir_path  + "/nvim"
 avim_user_path   = config_dir_path  + "/nvim/lua/user"
+font_dir_path    = "$HOME/.fonts"
 
 # Symlink paths
 #
@@ -30,7 +31,7 @@ def create_dir(dir):
 
 def copy_file(src, dest):
     print("Copying {} to {}".format(src,dest))
-    os.system("cp {} {}".format(src,dest))
+    os.system("cp -r {} {}".format(src,dest))
 
 def clone_repo(repo, dest):
     print("Git cloning {} to {}".format(repo,dest))
@@ -84,6 +85,13 @@ def install_configs():
     clone_repo("git@github.com:lukehayes/AstroNvimConfig.git", nvim_dir_path)
     new_line()
 
+    # Fonts
+    #
+    create_dir(font_dir_path)
+    copy_file("fonts/JetBrainsMono", font_dir_path)
+    copy_file("fonts/ShareTech", font_dir_path)
+    new_line()
+
 def nuke_all_configs():
         print("Nuking All Files...\n")
         remove("/home/luke/.config/dotfiles")
@@ -91,6 +99,7 @@ def nuke_all_configs():
         remove("/home/luke/.vimrc")
         remove("/home/luke/.vim")
         remove("/home/luke/.config/nvim")
+        remove("/home/luke/.fonts")
         print("All files removed.\n")
 
 def run_config():
